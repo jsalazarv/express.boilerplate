7aa107bf058c17e05a2a6856500b083085fda549
Juan Salazar <jsalazarv8@gmail.com>
2022-09-30 22:29:51 -0500

    ESLint and Prettier was configured

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = 3000;
@@ -6,12 +6,12 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/', (_, res) => {
-    res.json({
-        app:"Express Boilerplate Typescript",
-        version: "v1.0.0"
-    });
+  res.json({
+    app: 'Express Boilerplate Typescript',
+    version: 'v1.0.0',
+  });
 });
 
 app.listen(port, () => {
-    console.log(`The server is running on port: ${port}`)
+  console.log(`The server is running on port: ${port}`);
 });
